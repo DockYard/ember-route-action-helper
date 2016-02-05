1a71a8298c38a243d73c3dc55e1fbe75f0a07778
Robert Jackson <robert.w.jackson@me.com>
2016-02-05 10:39:14 -0500

    Make functions returned "real" actions.
    This prevents the extra/double mutable object wrapping when passing down
    from one layer to the next (and avoids requiring extra `(action`
    wrapping to prevent this).

--- addon/helpers/route-action.js
+++ addon/helpers/route-action.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import getOwner from 'ember-getowner-polyfill';
+import { ACTION } from '../-private/internals';
 
 const {
   A: emberArray,
@@ -37,12 +38,16 @@ export default Helper.extend({
     let router = get(this, 'router');
     assert('[ember-route-action-helper] Unable to lookup router', router);
 
-    return function(...invocationArgs) {
+    let action = function(...invocationArgs) {
       let args = params.concat(invocationArgs);
       let { action, handler } = getRouteWithAction(router, actionName);
       assert(`[ember-route-action-helper] Unable to find action ${actionName}`, handler);
 
       return action.apply(handler, args);
     };
+
+    action[ACTION] = true;
+
+    return action;
   }
 });
--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -10,6 +10,13 @@ export default function(name, options = {}) {
       if (options.beforeEach) {
         options.beforeEach.apply(this, arguments);
       }
+
+      this.register = (fullName, Factory) => {
+        let instance = this.application.__deprecatedInstance__;
+        let registry = instance.register ? instance : instance.registry;
+
+        return registry.register(fullName, Factory);
+      };
     },
 
     afterEach() {

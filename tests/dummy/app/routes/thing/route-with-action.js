DockYard/ember-route-action-helper

commit 0f0b2d3ad992e61e6dc8cb0b520fb3c3c1fa48e8

Fail immediately if route action is missing (#37)

addon/helpers/route-action.js
@@ -39,16 +39,16 @@ export default Helper.extend({
     let router = get(this, 'router');
     assert('[ember-route-action-helper] Unable to lookup router', router);
 
-    let action = function(...invocationArgs) {
-      let args = params.concat(invocationArgs);
-      let { action, handler } = getRouteWithAction(router, actionName);
-      assert(`[ember-route-action-helper] Unable to find action ${actionName}`, handler);
+    let { action, handler } = getRouteWithAction(router, actionName);
+    assert(`[ember-route-action-helper] Unable to find action ${actionName}`, handler);
 
+    let routeAction = function(...invocationArgs) {
+      let args = params.concat(invocationArgs);
       return run.join(handler, action, ...args);
     };
 
-    action[ACTION] = true;
+    routeAction[ACTION] = true;
 
-    return action;
+    return routeAction;
   }
 });

tests/acceptance/main-test.js
@@ -15,6 +15,7 @@ moduleForAcceptance('Acceptance | main', {
       }
     }));
     this.application.register('template:dynamic', hbs`{{parent-component go=(route-action 'foo') }}`);
+    this.application.register('template:dynamic2', hbs`{{parent-component go=(route-action 'notAnAction')}}`);
     this.application.register('template:components/parent-component', hbs`{{child-component go=go}}`);
     this.application.register('template:components/child-component', hbs`<button class="do-it">GO!</button>`);
     this.application.register('component:child-component', Component.extend({
@@ -53,4 +54,11 @@ test('it has a return value', function(assert) {
 test('it can be used without rewrapping with (action (route-action "foo"))', function() {
   visit('/dynamic');
   click('.do-it');
+});
+
+test('it should throw an error immediately if the route action is missing', function(assert) {
+  visit('/dynamic2');
+
+  let expectedResult = 'Assertion Failed: [ember-route-action-helper] Unable to find action notAnAction';
+  andThen().catch(({ message }) => assert.equal(message, expectedResult));
 });

tests/dummy/app/router.js
@@ -11,6 +11,7 @@ Router.map(function() {
     this.route('show');
   });
   this.route('dynamic');
+  this.route('dynamic2');
 });
 
 export default Router;

commit f2477c1e188b39085f0c42d34f295585e905f34c

Fixes an issue where actions were looked up too early
- Adds a test to cover this scenario
- This issue was accidentally introduced in #37

Fixes #49

addon/helpers/route-action.js
@@ -9,7 +9,8 @@ const {
   typeOf,
   get,
   getOwner,
-  run
+  run,
+  runInDebug
 } = Ember;
 
 function getRoutes(router) {
@@ -39,10 +40,13 @@ export default Helper.extend({
     let router = get(this, 'router');
     assert('[ember-route-action-helper] Unable to lookup router', router);
 
-    let { action, handler } = getRouteWithAction(router, actionName);
-    assert(`[ember-route-action-helper] Unable to find action ${actionName}`, handler);
+    runInDebug(() => {
+      let { handler } = getRouteWithAction(router, actionName);
+      assert(`[ember-route-action-helper] Unable to find action ${actionName}`, handler);
+    });
 
     let routeAction = function(...invocationArgs) {
+      let { action, handler } = getRouteWithAction(router, actionName);
       let args = params.concat(invocationArgs);
       return run.join(handler, action, ...args);
     };

tests/acceptance/main-test.js
@@ -66,3 +66,12 @@ test('it should throw an error immediately if the route action is missing', func
     done();
   });
 });
+
+test('it invokes action in the current route hierarchy', function(assert) {
+  visit('/thing');
+  click('.foo-button');
+  andThen(() => assert.equal(findWithAssert('.foo-value').text().trim(), 'Hello world Bob!'));
+  visit('/thing/route-with-action');
+  click('.foo-button');
+  andThen(() => assert.equal(findWithAssert('.foo-value').text().trim(), 'Set via route-with-action: Hello world Bob!'));
+});

tests/dummy/app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('thing', function() {
     this.route('show');
+    this.route('route-with-action');
   });
   this.route('dynamic');
   this.route('dynamic2');

tests/dummy/app/routes/thing/route-with-action.js
@@ -1,0 +1,12 @@
+import Ember from 'ember';
+
+const { Route, set } = Ember;
+
+export default Route.extend({
+  actions: {
+    updateFoo(...args) {
+      let applicationController = this.controllerFor('application');
+      return set(applicationController, 'foo', 'Set via route-with-action: ' + args.join(' '));
+    }
+  }
+});